9292dff38f8c20c1d7752f5802c905ecfe16890b
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-04-01 10:36:41 +0100

    Added function for encrypting text2morse

--- main.js
+++ main.js
@@ -61,3 +61,21 @@ function toggle() {
 				document.getElementById("convert").setAttribute('onclick','text2morse()');
 			}
 		}
+        
+// Function for encrypting the message 
+function text2morse() {	
+	let input = document.getElementById("input").value;
+	input = input.toUpperCase();
+	let arr1 = input.split("");
+	let arr2 = arr1.map(x => {
+		if(mapping[x])
+		{
+			return mapping[x];
+		} else {						
+			return x;
+		}
+	});
+	
+	let code = arr2.join(" ");
+	document.getElementById("output").value = code;	
+}
