b0b22e179db2c379247000689ae31971914a8d0f
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-04-01 10:35:50 +0100

    Added function for toggle button

--- main.js
+++ main.js
@@ -38,3 +38,26 @@ const mapping = {
     "8" : "---..", 
     "9" : "----."
 }
+
+// Function for toggle functionality
+function toggle() {
+	const decrypt = "Decrypt Message";
+	const encrypt = "Encrypt Message";
+	let current = document.getElementById("toggle").innerHTML;
+			if(current == decrypt)
+			{
+				document.getElementById("toggle").innerHTML = encrypt;
+				document.getElementById("input").value = "";
+				document.getElementById("output").value = "";
+				document.getElementById("input").placeholder = "Insert your morse code here...";
+				document.getElementById("convert").innerHTML = "Convert to Plain Text"
+				document.getElementById("convert").setAttribute('onclick','morse2text()');
+			} else {
+				document.getElementById("toggle").innerHTML = decrypt;
+				document.getElementById("input").value = "";
+				document.getElementById("output").value = "";
+				document.getElementById("input").placeholder = "Type your message here...";
+				document.getElementById("convert").innerHTML = "Convert to Morse Code"
+				document.getElementById("convert").setAttribute('onclick','text2morse()');
+			}
+		}
